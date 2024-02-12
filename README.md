19aac20b8814de94fba176186a184d858490fa51
emanrdesu <eperezpozo@knights.ucf.edu>
2024-02-12 15:23:12 -0500

    Update project settings and README

---
+++ README.md
@@ -0,0 +1,29 @@
+
+## dchan
+Is an anoymous imageboard with optional accounts that let you "attach" information about yourself to your post, such as gender, nationality, etc.
+
+> Status: Unfinished but functional.
+
+### Features
+1. Optional user accounts
+2. Themes
+3. No registration with email
+4. Modern look and feel
+
+#### Future
+- User messaging system
+- Local threads (set to a mile radius)
+- User verification for and by users
+
+## Stack/Tech
+- Svelte + SvelteKit
+- PostCSS + DaisyUI
+- Zod + SvelteKit Superforms
+- Pocketbase for database hosting
+- To be hosted in a Linode server
+
+## Screenshots
+![Homepage](/static/homepage.png)
+![Board](/static/board.png)
+![Post](/static/post.png)
+![Thread](/static/thread.png)
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,18 +1,42 @@
 const config = {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+   content: ['./src/**/*.{html,js,svelte,ts}'],
 
-	theme: {
-		extend: {}
-	},
+   theme: {},
 
-	plugins: [
-		require("daisyui")
-	],
+   plugins: [require('daisyui')],
 
-	daisyui: {
-		prefix: "ds",
-		themes: ["dracula", "autumn"],
-	}
-};
+   daisyui: {
+      themes: [
+         {
+            dracula: {
+               ...require('daisyui/src/colors/themes')['[data-theme=dracula]'],
+               primary: '#ff99d3',
+               'primary-focus': '#e28399',
+               'secondary-focus': '#a76ef7',
+               neutral: '#313444',
+               'neutral-focus': '#3c3f53',
+               '--tw-text-opacity': '0.85'
+            }
+         },
 
-module.exports = config;
+         {
+            mars: {
+               primary: '#d57676',
+               'primary-focus': '#cd5c5c',
+               secondary: '#6dbbc4',
+               accent: '#a08bda',
+               neutral: '#332834',
+               'base-100': 'rgb(41, 32, 42)',
+
+               // 'base-100': '#413441',
+               info: '#818cf8',
+               success: '#38E595',
+               warning: '#F7A14B',
+               error: '#be123c'
+            }
+         }
+      ]
+   }
+}
+
+module.exports = config
